fcb29dcf2cf2e1a183dc242a60b8cf4c81ab022a
jomwfz <6710210096@psu.ac.th>
2026-02-17 02:16:38 +0700

    GuestRoom / ห้องหารตัง v.1

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,9 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from './App.jsx'
 import GuestPay from './GuestPay.jsx'
 
-// ✅ ใช้ import.meta.env.BASE_URL เพื่อดึงค่า base จาก vite.config.js อัตโนมัติ
-// หรือจะใส่เป็น basename="/fair-split" ตรงๆ ก็ได้ครับ
-
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename="/fair-split">  {/* 👈 เพิ่มตรงนี้ครับ */}
+    <BrowserRouter> 
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/pay/:roomId" element={<GuestPay />} />
